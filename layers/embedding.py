fix imports, add backpropagation jupyter notebook for detailed explanation

layers/embedding.py
@@ -57,6 +57,6 @@ class EmbeddingLayer(nn.Module):
         token_emb = self.token_emb(x)
         # works as a look up table, returns needed positions embeddings
         # CONTEXT_SIZE x EMBEDDING_DIM
-        positional_emb = self.positional_emb(token_emb)
+        input_emb = self.positional_emb(token_emb)
 
-        return positional_emb
+        return input_emb

train.py
@@ -2,7 +2,7 @@ from models.model import GPT2
 from torch import nn
 from torch.optim import AdamW
 from wb.wandb_config import init_wandb, log_wandb
-from tokenizer.BPETokenizer import BPETokenizer
+from tokenizer import BPETokenizer
 from torch.utils.data import DataLoader
 from hyperparams import EPOCH_NUMBER, LEARNING_RATE, BATCH_SIZE
 from datasets import TextDataset
@@ -32,14 +32,16 @@ def train():
             model.train()
 
             logits = model.forward(inputs)
-            loss = criterion(logits)
+            logits = logits.view(
+                -1, logits.size(-1)
+            )  # reshape to [batch_size*sequence_length, vocab_size]
+            target = target.view(-1)
+            loss = criterion(logits, target)
 
+            # loss is a final node of a graph
+            # see details inside experiments/backprop.ipynb
             loss.backward()
 
             optimizer.step()
 
             total_loss += loss.item()
-
-
-tokenizer = BPETokenizer()
-dataset = TextDataset(tokenizer)